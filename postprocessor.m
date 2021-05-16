baf95b9f2815bdeb898a74a5e1a22eae7a1b3265
norbertprokopiuk <61018774+norbertprokopiuk@users.noreply.github.com>
2021-05-16 13:13:23 +0200

    postprocessor update

--- postprocessor.m
+++ postprocessor.m
@@ -62,50 +62,68 @@ plot(T(1,:),Q(3*temp-2,:));
 grid on
 legend("x");
 title("Przemieszczenie punktu w osi x");
+xlabel("Czas [s]")
+ylabel("Przemieszczenie [m]")
 
 figure
 plot(T(1,:),Q(3*temp-1,:))
 grid on
 legend("y");
 title("Przemieszczenie punktu w osi y");
+xlabel("Czas [s]")
+ylabel("Przemieszczenie [m]")
 
 figure
 plot(Q(3*temp-2,:),Q(3*temp-1,:))
 title("Trajektoria punktu")
 grid on
+xlabel("Przemieszczenie w osi x [m]")
+ylabel("Przemieszczenie w osi y[m]")
 
 figure
 plot(T(1,:),DQ(3*temp-2,:));
 title("Predkość w osi x")
 grid on
 legend("x");
+xlabel("Czas [s]")
+ylabel("Predkosc [m/s]")
 figure
 plot(T(1,:),DQ(3*temp-1,:))
 title("Predkość w osi y")
 grid on
 legend("y");
+xlabel("Czas [s]")
+ylabel("Predkosc [m/s]")
 
 figure
 plot(T(1,:),D2Q(3*temp-2,:));
 grid on
 title("Przyspieszenie w osi x")
 legend("x");
+xlabel("Czas [s]")
+ylabel("Przyspieszenie [m/s^2]")
 figure
 plot(T(1,:),D2Q(3*temp-1,:))
 grid on
 legend("y");
 title("Przyspieszenie w osi y")
+xlabel("Czas [s]")
+ylabel("Przyspieszenie [m/s^2]")
 
 figure
-plot(T(1,:),DQ(3*temp,:));
+plot(T(1,:),180*DQ(3*temp,:)/pi);
 grid on
 title("Predkosc katowa członu")
 legend("x");
+xlabel("Czas [s]")
+ylabel("Prędkosć kątowa [stopnie/s]")
 figure
-plot(T(1,:),D2Q(3*temp,:))
+plot(T(1,:),180*D2Q(3*temp,:)/pi)
 grid on
 legend("y");
 title("Przyspieszenie katowe członu")
+xlabel("Czas [s]")
+ylabel("Przyspieszeni kątowa [stopnie/s^2]")
 end
 
 if ktory_punkt(1)=='p'
@@ -119,15 +137,21 @@ plot(T(1,:),nowe_punkty(3*temp-2,:));
 grid on
 legend("x");
 title("Przemieszczenie punktu w osi x");
+xlabel("Czas [s]")
+ylabel("Przemieszczenie [m]")
 
 figure
 plot(T(1,:),nowe_punkty(3*temp-1,:))
 grid on
 legend("y");
 title("Przemieszczenie punktu w osi y");
+xlabel("Czas [s]")
+ylabel("Przemieszczenie [m]")
 
 figure
 plot(nowe_punkty(3*temp-2,:),nowe_punkty(3*temp-1,:))
 title("Trajektoria punktu")
 grid on
+xlabel("Przemieszczenie w osi x [m]")
+ylabel("Przemieszczenie w osi y[m]")
 end
